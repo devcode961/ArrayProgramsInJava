670a27b2defacfe99d33920ab5d60e204cd1aa92
GoldDevCode <swarnimb6@gmail.com>
2022-01-01 19:40:53 +0100

    Initial commit

---
+++ src/com/dev/array/ArrayReverse.java
@@ -0,0 +1,18 @@
+package com.dev.array;
+
+import java.util.Arrays;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        System.out.println("Program to reverse array");
+        int []num_arr = {1,2,3,4,5,6,7};
+        int array_size = num_arr.length-1;
+        int pivot_index = array_size/2;
+        for(int i=0;i<=pivot_index;i++){
+            int temp = num_arr[i];
+            num_arr[i] = num_arr[array_size - i];
+            num_arr[array_size - i] = temp;
+        }
+         System.out.println("Reversed array is : "+Arrays.toString(num_arr));
+    }
+}
