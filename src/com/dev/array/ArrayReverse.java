d31163449df0efe4ae85785ad392188b6fdd35e2
devcode961 <devcode961@gmail.com>
2022-01-01 19:49:02 +0100

    Change in array data

--- src/com/dev/array/ArrayReverse.java
+++ src/com/dev/array/ArrayReverse.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ArrayReverse {
     public static void main(String[] args) {
         System.out.println("Program to reverse array");
-        int []num_arr = {1,2,3,4,5,6,7};
+        int []num_arr = {1,2,3,4,5,6,7,8,9,10};
         int array_size = num_arr.length-1;
         int pivot_index = array_size/2;
         for(int i=0;i<=pivot_index;i++){
