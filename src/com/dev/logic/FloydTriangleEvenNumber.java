109944a82092dfb73c802b543551a1c4fff9bd97
devcode961 <devcode961@gmail.com>
2022-02-20 15:32:11 +0100

    Printing the Floyd Triangle Pattern for Even and Odd numbers

---
+++ src/com/dev/logic/FloydTriangleEvenNumber.java
@@ -0,0 +1,14 @@
+package com.dev.logic;
+
+public class FloydTriangleEvenNumber {
+    public static void main(String[] args) {
+        int k = 1;
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(2*k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
