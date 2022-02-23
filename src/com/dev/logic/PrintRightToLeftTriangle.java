5844c12a12e7c4f0fb529d2992d44ed67138f568
devcode961 <devcode961@gmail.com>
2022-02-23 18:56:21 +0100

    Removed extra variable k from code, instead using loop variable i can work well.

--- src/com/dev/logic/PrintRightToLeftTriangle.java
+++ src/com/dev/logic/PrintRightToLeftTriangle.java
@@ -10,18 +10,16 @@ This logic will print the Triangle Pattern from Right to left with spacing betwe
 public class PrintRightToLeftTriangle {
 
     public static void main(String[] args) {
-        int size = 10;
-        int k = 1;
-        for (int i=1;i<=5;i++){
+        int size = 18;
+        for (int i=1;i<=9;i++){
             for (int j=1;j<=size;j++){
                 if(j<=(size-2*i)){
                     System.out.print(" ");
                 }
                 else{
-                    System.out.print(j%2 != 0 ? " " : k+"");
+                    System.out.print(j%2 != 0 ? " " : i+"");
                 }
             }
-            k++;
             System.out.println();
         }
     }
