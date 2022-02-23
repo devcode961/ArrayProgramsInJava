9df3e98cb404decfeb6ed0c6302e42c031eb0430
devcode961 <devcode961@gmail.com>
2022-02-23 16:44:18 +0100

    Printing right to left triangle in ascending order.

--- src/com/dev/logic/PrintRightToLeftTriangle.java
+++ src/com/dev/logic/PrintRightToLeftTriangle.java
@@ -1,15 +1,29 @@
 package com.dev.logic;
-
+/*
+This logic will print the Triangle Pattern from Right to left with spacing between numbers like below :
+         1
+       2 2
+     3 3 3
+   4 4 4 4
+ 5 5 5 5 5
+ */
 public class PrintRightToLeftTriangle {
+
     public static void main(String[] args) {
+        int size = 10;
         int k = 1;
         for (int i=1;i<=5;i++){
-            for (int j=5;j>=1;j--){
-                if(j<=k){
-                    System.out.print(k);
+            for (int j=1;j<=size;j++){
+                if(j<=(size-2*i)){
+                    System.out.print(" ");
                 }
                 else{
-                    System.out.print(" ");
+                    if(j%2!=0){
+                        System.out.print(" ");
+                    }
+                    else{
+                        System.out.print(k);
+                    }
                 }
             }
             k++;
